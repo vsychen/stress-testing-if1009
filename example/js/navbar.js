c825154fb5736efe1f5a45c777551e129bb6e1ed
Victor Chen <vsyc@cin.ufpe.br>
2017-10-10 18:00:43 -0300

    Changing example files location
    from stress-testing-if10009 to stress-testing-if1009/example

---
+++ example/js/navbar.js
@@ -0,0 +1,11 @@
+$('.toggle').click(function () {
+  "use strict";
+  $('nav ul').slideToggle();
+});
+
+$(window).resize(function () {
+  "use strict";
+  if ($(window).width() > 780) {
+    $('nav ul').removeAttr('style');
+  }
+});
